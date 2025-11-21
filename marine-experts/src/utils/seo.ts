6a8f40f70d09360d0c8c8530c6d829127cee3b8a
Abdelrahman Elshouny <elshouny7@gmail.com>
2025-11-21 14:27:07 +0200

    Changed Logo

--- marine-experts/src/utils/seo.ts
+++ marine-experts/src/utils/seo.ts
@@ -9,7 +9,7 @@ export function orgJsonLd(site: string) {
     '@id': `${canonical}#organization`,
     name: 'Marine Experts',
     url: canonical,
-    logo: `${canonical}/icons/logo.svg`,
+    logo: `${canonical}/icons/logo-navy.svg`,
     // Fill these in when you have profiles
     // sameAs: [
     //   'https://www.linkedin.com/company/marine-experts',
